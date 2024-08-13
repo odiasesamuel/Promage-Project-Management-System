bc138225891244c72e5dc02fb84da9df57b9c978
odiase samuel <odiasesamuel4@gmail.com>
2024-08-13 20:34:51 +0100

    change project name to promage

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+// const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
+
+const nextConfig = {
+  env: {
+    SMTP_HOST: "smtp.gmail.com",
+    SMTP_PORT: "465",
+    SMTP_USER: "codewithodiase@gmail.com",
+    SMTP_PASS: "umbqordbeprkedhu",
+  },
+};
+
+
+// SMTP_HOST=smtp.gmail.com
+// SMTP_PORT=465
+// SMTP_USER=codewithodiase@gmail.com
+// SMTP_PASS=umbqordbeprkedhu
 
 export default nextConfig;
