5ed150dbfaf806c88cb3957dba9daf54edde5b97
odiasesamuel <odiasesamuel4@gmail.com>
2024-09-02 22:14:44 +0100

    clean up projest page and use the context implementation for data on the project page

--- app/(application)/project/page.tsx
+++ app/(application)/project/page.tsx
@@ -1,29 +1,13 @@
-import { columns, ProjectListType } from "../../../components/columns";
-import { getProjectSummary } from "@/lib/dashboard";
-import { redirect } from "next/navigation";
-import { verifyAuth } from "@/lib/auth";
-import { getEmployeeByEmployeeId, getAllEmployee } from "@/lib/employee";
-import { EmployeeSignInDetailsType } from "@/actions/auth-action";
+import { columns } from "../../../components/columns";
 import { DataTable } from "../../../components/data-table";
 import { Card, CardContent } from "@/components/ui/card";
-import { EmployeeListType } from "../layout";
 
 const ProjectPage = async () => {
-  const result = await verifyAuth();
-  if (!result.user) {
-    return redirect("/");
-  }
-  const employeeDetails: EmployeeSignInDetailsType = await getEmployeeByEmployeeId(result.user.id);
-  const organisation_id = employeeDetails.organisation_id;
-  const employeeList: EmployeeListType[] = await getAllEmployee(organisation_id);
-
-  const projectList: ProjectListType[] = await getProjectSummary(organisation_id);
-
   return (
     <>
       <Card className="bg-[#F2EAE5] mt-12">
         <CardContent>
-          <DataTable columns={columns} data={projectList} employeeListData={employeeList} className="" />
+          <DataTable columns={columns} />
         </CardContent>
       </Card>
     </>
