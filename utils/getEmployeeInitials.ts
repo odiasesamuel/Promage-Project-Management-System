0cdb02ef5509cc7de7e5ea94328478e37642d372
odiasesamuel <odiasesamuel4@gmail.com>
2024-09-04 00:43:43 +0100

    implement functionality to capitalise initials

--- utils/getEmployeeInitials.ts
+++ utils/getEmployeeInitials.ts
@@ -1,7 +1,7 @@
 export const getEmployeeInitials = (fullName: string) => {
   const names = fullName.split(" ");
-  const firstNameInitial = names[0] ? names[0][0] : "";
-  const lastNameInitial = names.length > 1 ? names[names.length - 1][0] : "";
+  const firstNameInitial = names[0] ? names[0][0].toUpperCase() : "";
+  const lastNameInitial = names.length > 1 ? names[names.length - 1][0].toUpperCase() : "";
 
   return `${firstNameInitial}${lastNameInitial}`;
 };
